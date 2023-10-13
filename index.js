5130035a94d26e14ab3d0939698ea18ddceb79fc
Sushil97 <sushil500530@gmail.com>
2023-10-13 22:39:19 +0600

    get post method from client side

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("coffeeDB")
+    const coffeeCollection = database.collection("coffee")
+
+    app.get('/coffee', async(req,res) => {
+        const findCoffee = coffeeCollection.find();
+        const result = await findCoffee.toArray();
+        res.send(result)
+    })
+
+app.post('/coffee', async(req,res) => {
+    const newCoffee = req.body;
+    console.log(newCoffee);
+    const result = await coffeeCollection.insertOne(newCoffee) ;
+    res.send(result)
+})
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
